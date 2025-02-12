935112e500f44583d508808bb19bf33fdde76124
Oleksandr Kovaliv <oleksandr.p.kovaliv@globallogic.com>
2025-02-12 10:15:19 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,178 @@
+mod movement;
+
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy_rapier2d::{plugin::{NoUserData, RapierPhysicsPlugin}, prelude::{ActiveEvents, Collider, ExternalForce, Friction, GravityScale, LockedAxes, RigidBody, Velocity}};
+use movement::*;
+
+const FLOOR_STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const LEFT_WALL_STARTING_POSITION: Vec3 = Vec3::new(-600.0, -200.0, 1.0);
+const RIGHT_WALL_STARTING_POSITION: Vec3 = Vec3::new(600.0, -200.0, 1.0);
+
+const BALL_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
+// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, 150.0, 1.0);
+pub const BALL_DIAMETER: f32 = 30.;
+const CAMERA_TRANSFORM: Vec3 = Vec3::new(0.0, 3.0, 0.0);
+const CAMERA_DECAY_RATE: f32 = 10.;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(125.0))
+        .add_systems(Startup, (
+            setup, 
+            setup_floor
+        ))
+        .add_systems(Update, (
+            cweampuf_move,
+            cweampuf_jump,
+            cweampuf_dash,
+            cweampuf_camera_adjustment
+        ))
+        .add_systems(FixedUpdate, (
+            dash_reset,
+            jump_reset,
+            velocity_limiter,
+            stunlock_reset,
+        ))
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    //asset_server: Res<AssetServer>,
+) {
+    let mut projection = OrthographicProjection::default_2d();
+    projection.scaling_mode = ScalingMode::AutoMin { min_width: 1920., min_height: 1080. };
+
+    // Cweampuf
+    commands.spawn((
+        RigidBody::Dynamic,
+        Mesh2d(meshes.add(Circle::default())),
+        MeshMaterial2d(materials.add(BALL_COLOR)),
+        Transform::from_translation(BALL_STARTING_POSITION).with_scale(Vec2::splat(BALL_DIAMETER).extend(1.)),
+        Cweampuf,
+        Velocity {
+            linvel: Vec2::new(0.0, 0.0),
+            angvel: 0.,
+        },
+        GravityScale(1.5),
+        Friction::coefficient(0.7),
+        Collider::ball(0.5),
+        Jumper { jump_impulse: 800., is_jump_available: true, is_jumping: false, is_next_jump_doublejump: false, is_double_jump_available: true},
+        Dasher { dash_impulse: 650., dash_cooldown: 1., time_passed_since_dash: 0. },
+        LockedAxes::ROTATION_LOCKED,
+        Movable { facing_right: true, hugging_wall: false, is_stunlocked: false, stun_duration: 0.2, time_passed_since_stun: 0. },
+        ExternalForce::default(),
+    ));
+    
+    // Camera
+    commands.spawn((
+        Camera2d,
+        Camera{
+            ..default()
+        },
+        Transform::from_translation(CAMERA_TRANSFORM),
+        CameraUpDownMovalbe { look_up_down_duration: 0., look_up_down_invoke_threshold: 0.3, camera_offset: 360. },
+        projection
+    ));
+}
+
+fn setup_floor (
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    //asset_server: Res<AssetServer>,
+) {
+    let floor_width = 1400.;
+    let floor_height = 100.;
+
+    commands
+        .spawn(RigidBody::Fixed)
+        .insert((
+            Mesh2d(meshes.add(Rectangle::new(floor_width, floor_height))),
+            MeshMaterial2d(materials.add(BALL_COLOR)),
+            Transform::from_translation(FLOOR_STARTING_POSITION)
+        ))
+        .insert(Collider::cuboid(floor_width / 2.0, floor_height / 2.0))
+        .insert(Friction::coefficient(0.7))
+        .insert(ActiveEvents::COLLISION_EVENTS)
+        .insert(FloorCollider {entity_index: 0});
+
+    let left_wall_width = 100.;
+    let left_wall_height = 2000.;
+
+    commands
+        .spawn(RigidBody::Fixed)
+        .insert((
+            Mesh2d(meshes.add(Rectangle::new(left_wall_width, left_wall_height))),
+            MeshMaterial2d(materials.add(BALL_COLOR)),
+            Transform::from_translation(LEFT_WALL_STARTING_POSITION)
+        ))
+        .insert(Collider::cuboid(left_wall_width / 2.0, left_wall_height / 2.0))
+        .insert(Friction::coefficient(0.7))
+        .insert(ActiveEvents::COLLISION_EVENTS)
+        .insert(FloorCollider {entity_index: 0});
+
+    commands
+        .spawn(RigidBody::Fixed)
+        .insert((
+            Mesh2d(meshes.add(Rectangle::new(left_wall_width, left_wall_height))),
+            MeshMaterial2d(materials.add(BALL_COLOR)),
+            Transform::from_translation(RIGHT_WALL_STARTING_POSITION)
+        ))
+        .insert(Collider::cuboid(left_wall_width / 2.0, left_wall_height / 2.0))
+        .insert(Friction::coefficient(0.7))
+        .insert(ActiveEvents::COLLISION_EVENTS)
+        .insert(FloorCollider {entity_index: 0});
+}
+
+fn cweampuf_camera_adjustment(
+    keyboard_input: Res<ButtonInput<KeyCode>>, 
+    cweampuf: Single<&Transform, (With<Cweampuf>, Without<Camera2d>)>,
+    mut camera: Single<(&mut Transform, &mut CameraUpDownMovalbe), With<Camera2d>>,
+    time: Res<Time>,
+) {
+    let (camera_transform, camera_movable) = &mut *camera;
+
+    if keyboard_input.just_released(KeyCode::ArrowUp) || keyboard_input.just_released(KeyCode::ArrowDown) {
+        camera_movable.look_up_down_duration = 0.;
+    }
+
+    let mut direction = 1.;
+
+    if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::ArrowDown) {
+        if camera_movable.look_up_down_duration < camera_movable.look_up_down_invoke_threshold {
+            camera_movable.look_up_down_duration += time.delta_secs();
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            direction = -1.;
+        }
+    }
+
+    let mut offset = Vec3::new(0., 0., 0.);
+
+    if camera_movable.look_up_down_duration >= camera_movable.look_up_down_invoke_threshold {
+        offset.y = camera_movable.camera_offset * direction;
+    }
+
+    camera_transform.translation.smooth_nudge(&(cweampuf.translation + offset + CAMERA_TRANSFORM), CAMERA_DECAY_RATE, time.delta_secs());
+}
+
+#[derive(Component)]
+struct Cweampuf;
+
+#[derive(Component)]
+struct FloorCollider {
+    entity_index: u32
+}
+
+#[derive(Component)]
+struct CameraUpDownMovalbe {
+    look_up_down_invoke_threshold: f32,
+    look_up_down_duration: f32,
+    camera_offset: f32,
+}
---
+++ src/movement.rs
@@ -0,0 +1,253 @@
+use bevy::{ecs::observer::TriggerTargets, math::bounding::{Aabb2d, BoundingCircle, BoundingVolume}, prelude::*};
+use bevy_rapier2d::prelude::{Collider, CollisionEvent, Velocity};
+
+use crate::{Cweampuf, FloorCollider, BALL_DIAMETER};
+
+const CWEAMPUF_SPEED: f32 = 500.0;
+const MAX_CWEAMPUF_VERTICAL_VELOCITY: f32 = 800.0;
+const MAX_DASH_IMPULSE: f32 = 1250.0;
+const MAX_WALL_DESCEND_VELOCITY: f32 = 200.0;
+
+#[derive(Component)]
+pub struct Jumper {
+    pub jump_impulse: f32,
+    pub is_jumping: bool,
+    pub is_jump_available: bool,
+    pub is_next_jump_doublejump: bool,
+    pub is_double_jump_available: bool
+}
+
+#[derive(Component)]
+pub struct Dasher {
+    pub dash_impulse: f32,
+    pub dash_cooldown: f32,
+    pub time_passed_since_dash: f32
+}
+
+#[derive(Component)]
+pub struct Movable {
+    pub facing_right: bool,
+    pub hugging_wall: bool,
+    pub is_stunlocked: bool,
+    pub stun_duration: f32,
+    pub time_passed_since_stun: f32
+}
+
+pub fn cweampuf_move(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut cweampuf_transform_velocity: Single<(&mut Velocity, &mut Movable), With<Cweampuf>>) {
+    let mut direction;
+    let (cweampuf_velocity, cweampuf_movable) = &mut *cweampuf_transform_velocity;
+
+    if cweampuf_movable.is_stunlocked {
+        return;
+    }
+
+    if keyboard_input.just_released(KeyCode::ArrowLeft) || keyboard_input.just_released(KeyCode::ArrowRight) {
+        cweampuf_velocity.linvel.x = 0.;
+    } 
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        if cweampuf_movable.hugging_wall && !cweampuf_movable.facing_right {
+            cweampuf_velocity.linvel.x = 0.;
+            return;
+        }
+
+        direction = -1.0;
+        cweampuf_movable.facing_right = false;
+
+        let new_velocity = direction * CWEAMPUF_SPEED;
+
+        if cweampuf_velocity.linvel.x > new_velocity {
+            cweampuf_velocity.linvel.x = new_velocity;
+        }
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        if cweampuf_movable.hugging_wall && cweampuf_movable.facing_right {
+            cweampuf_velocity.linvel.x = 0.;
+            return;
+        }
+
+        direction = 1.0;
+        cweampuf_movable.facing_right = true;
+
+        let new_velocity = direction * CWEAMPUF_SPEED;
+
+        if cweampuf_velocity.linvel.x < new_velocity {
+            cweampuf_velocity.linvel.x = new_velocity;
+        }
+    }
+}
+
+pub fn cweampuf_jump(keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut cweampuf_jumper: Single<(&mut Jumper, &mut Velocity, &mut Movable), With<Cweampuf>>) {
+    let (jumper, velocity, movable) = &mut *cweampuf_jumper;
+
+    let jump_released = keyboard_input.just_released(KeyCode::Space);
+
+    if jump_released {
+        jumper.is_next_jump_doublejump = true && jumper.is_double_jump_available;
+        jumper.is_jumping = true;
+        jumper.is_double_jump_available = false;
+        jumper.is_jump_available = true;
+    }
+
+    if movable.is_stunlocked {
+        return;
+    }
+
+    if jump_released {
+        // Kill current vertical velocity 
+        if velocity.linvel.y > 0.0 {
+            velocity.linvel.y = 0.;
+        }
+    }
+
+    if keyboard_input.pressed(KeyCode::Space) && jumper.is_jump_available && (!jumper.is_jumping || jumper.is_next_jump_doublejump) {
+        velocity.linvel.y = jumper.jump_impulse;
+        jumper.is_jumping = true;
+        jumper.is_jump_available = false;
+    
+        if movable.hugging_wall {
+            let wall_jump_hor_velocity;
+    
+            if movable.facing_right {
+                wall_jump_hor_velocity = -CWEAMPUF_SPEED;
+                movable.facing_right = false;
+            }
+            else {
+                movable.facing_right = true;
+                wall_jump_hor_velocity = CWEAMPUF_SPEED;
+            }
+    
+            movable.is_stunlocked = true;
+            velocity.linvel.x = wall_jump_hor_velocity;
+        }
+    
+        if jumper.is_next_jump_doublejump {
+            jumper.is_next_jump_doublejump = false;
+        }
+    }
+}
+
+pub fn cweampuf_dash(
+    keyboard_input: Res<ButtonInput<KeyCode>>, 
+    mut cweampuf_dasher: Single<(&mut Dasher, &mut Velocity, &mut Movable), With<Cweampuf>>) {
+    let (dasher, velocity, movable) = &mut *cweampuf_dasher;
+
+    if movable.is_stunlocked {
+        return;
+    }
+
+    if !keyboard_input.just_pressed(KeyCode::KeyX) || dasher.dash_cooldown - dasher.time_passed_since_dash > 0.01  {
+        return;
+    }
+
+    let mut vertical_velocity = velocity.linvel.y;
+
+    if vertical_velocity < 0.0 {
+        vertical_velocity = 0.0;
+    }
+ 
+    let mut dash_impulse = dasher.dash_impulse;
+
+    if !movable.facing_right {
+        dash_impulse = -dash_impulse;
+    }
+
+    velocity.linvel = Vec2::new((dash_impulse + velocity.linvel.x).clamp(-MAX_DASH_IMPULSE, MAX_DASH_IMPULSE), vertical_velocity).into();
+
+    dasher.time_passed_since_dash = 0.;
+}
+
+pub fn velocity_limiter(mut cweampuf: Single<(&mut Velocity, &Movable), With<Cweampuf>>) {
+    let (cweampuf_velocity, cweampuf_movable) = &mut *cweampuf;
+
+    if cweampuf_movable.hugging_wall && cweampuf_velocity.linvel.y < 0. {
+        cweampuf_velocity.linvel.y = -MAX_WALL_DESCEND_VELOCITY;
+    }
+
+    cweampuf_velocity.linvel.y = cweampuf_velocity.linvel.y.clamp(-MAX_CWEAMPUF_VERTICAL_VELOCITY, MAX_CWEAMPUF_VERTICAL_VELOCITY);
+}
+
+pub fn dash_reset(mut cweampuf_dasher: Single<&mut Dasher, With<Cweampuf>>, time: Res<Time>) {
+    if cweampuf_dasher.time_passed_since_dash <= cweampuf_dasher.dash_cooldown {
+        cweampuf_dasher.time_passed_since_dash += time.delta_secs();
+    } 
+}
+
+pub fn stunlock_reset(mut cweampuf_movable: Single<&mut Movable, With<Cweampuf>>, time: Res<Time>) {
+    if !cweampuf_movable.is_stunlocked {
+        return;
+    }
+
+    if cweampuf_movable.time_passed_since_stun <= cweampuf_movable.stun_duration {
+        cweampuf_movable.time_passed_since_stun += time.delta_secs();
+        return;
+    }
+
+    cweampuf_movable.is_stunlocked = false;
+    cweampuf_movable.time_passed_since_stun = 0.;
+}
+
+pub fn jump_reset(
+    mut cweampuf: Single<(Entity, &mut Jumper, &mut Movable, &Transform), With<Cweampuf>>,
+    mut colliders: Query<(Entity, &Transform, &Collider, &mut FloorCollider), With<FloorCollider>>,
+    mut contact_events: EventReader<CollisionEvent>) {
+    let (cweampuf_entity, cweampuf_jumper, cweampuf_movable, cweampuf_transform) = &mut *cweampuf;
+
+    for contact_event in contact_events.read() {
+        detect_floor_and_wall_collision(*cweampuf_entity, cweampuf_jumper, cweampuf_transform, cweampuf_movable, contact_event, &mut colliders);
+    }
+}
+
+fn detect_floor_and_wall_collision(cweampuf_entity: Entity, 
+                                   jumper: &mut Jumper, 
+                                   cweampuf_transform: &Transform,
+                                   cweampuf_movable: &mut Movable,
+                                   event: &CollisionEvent, 
+                                   colliders: &mut Query<(Entity, &Transform, &Collider, &mut FloorCollider), With<FloorCollider>>) {
+    if cweampuf_movable.hugging_wall {
+        if let CollisionEvent::Stopped(h1, h2, _flags) = event {
+            for (collider_entity, _collider_transform, _collider, mut floor_collider) in colliders.iter_mut() {
+                if h1.entities().iter().any(|f| *f == collider_entity || *f == cweampuf_entity) && 
+                   h2.entities().iter().any(|f| *f == collider_entity || *f == cweampuf_entity) &&
+                   floor_collider.entity_index != 0 {
+                    cweampuf_movable.hugging_wall = false;
+                    floor_collider.entity_index = 0;
+                }
+            }
+        }
+    }
+
+    if let CollisionEvent::Started(h1, h2, _flags) = event {
+        for (collider_entity, collider_transform, collider, mut floor_collider) in colliders.iter_mut() {
+            if h1.entities().iter().any(|f| *f == collider_entity || *f == cweampuf_entity) && 
+               h2.entities().iter().any(|f| *f == collider_entity || *f == cweampuf_entity) {
+                jumper.is_jumping = false;
+                jumper.is_next_jump_doublejump = false;
+                jumper.is_double_jump_available = true;
+
+                if let Some(cuboid) = collider.as_cuboid() {
+                    let cweampuf_bounds = BoundingCircle::new(cweampuf_transform.translation.truncate(), BALL_DIAMETER / 2.);
+                    let collider_bounds = Aabb2d::new(collider_transform.translation.truncate(),
+                                                              cuboid.half_extents());
+
+                    if check_wall_collision(cweampuf_bounds, collider_bounds) {
+                        cweampuf_movable.hugging_wall = true;
+                        floor_collider.entity_index = collider_entity.index();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn check_wall_collision(cweampuf_bounds: BoundingCircle, wall_bounds: Aabb2d) -> bool {
+    let closest = wall_bounds.closest_point(cweampuf_bounds.center());
+    let offset = cweampuf_bounds.center() - closest;
+
+    return offset.x.abs() >= offset.y.abs();
+}
